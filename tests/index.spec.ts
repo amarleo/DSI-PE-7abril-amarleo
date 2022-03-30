e7e2a1de04222e043b968f91f1518933969e78d0
Alejandro Martín de León <alu01015941@ull.edu.es>
2022-03-30 21:23:45 +0000

    tests pass example

---
+++ tests/index.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import {expect} from 'chai';
+import {index} from '../src/index';
+
+describe('Index Class Tests', () => {
+  it('', () => {
+    expect(index()).to.be.eql('prueba');
+  });
+});
