a2b2b37e0f30294aaa7865a950bc7e48150794cc
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-31 10:46:47 +0100

    doc done

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,21 +1,27 @@
 import {ImperialLength} from './imperialLength';
 import {MetricalLength} from './metricalLength';
 
+/**
+ * Adaptr class
+ */
 export class Adapter extends ImperialLength {
   constructor(private service: MetricalLength) {
     super();
   }
+  /**
+   * Transform plg into cm
+   * @returns cms
+   */
   getData(): number {
     return this.service.getLength() * 2.54;
   }
 }
 
-const metrical = new MetricalLength(25);
+// const metrical = new MetricalLength(25);
 
-function clientCode(data: number) {
-  console.log(data);
-}
+// function clientCode(data: number) {
+//   console.log(data);
+// }
 
-
-const adapter = new Adapter(metrical);
-clientCode(adapter.getData());
+// const adapter = new Adapter(metrical);
+// clientCode(adapter.getData());
--- src/metricalLength.ts
+++ src/metricalLength.ts
@@ -1,15 +1,30 @@
+/**
+ * Metrical Length Class
+ */
 export class MetricalLength {
   constructor(private length: number = 0) {
   }
 
+  /**
+   * gets the length data
+   * @returns number on cm
+   */
   getLength(): number {
     return this.length;
   }
 
+  /**
+   * gets the meters
+   * @returns transform cm into m
+   */
   toM(): number {
     return this.length / 100;
   }
 
+  /**
+   * gets the kilometers
+   * @returns transform cm into km
+   */
   toKm(): number {
     return this.length / 1000;
   }
