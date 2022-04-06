8aef4084013474af532f747fd39920fed6f3e8e3
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-04-06 18:25:45 +0100

    example 1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-export function Index(): string {
-  return 'index';
+/**
+ * Imperial Length Class
+ */
+ export class ImperialLength {
+  constructor(private length: number = 0) {
+  }
+
+  /**
+   * gets length in plgs
+   * @returns pulgs
+   */
+  getLength(): number {
+    return Math.round(this.length);
+  }
+
+  /**
+   * get number of feets
+   * @returns number of feets
+   */
+  toFt(): number {
+    return Math.round(this.length / 12);
+  }
+
+  /**
+   * plg into Yd method
+   * @returns number of yds
+   */
+  toYd(): number {
+    return Math.round(this.length / 36);
+  }
+
+  /**
+   * plg into millas method
+   * @returns number of millas
+   */
+  toMillas(): number {
+    return Math.round(this.length / 63360);
+  }
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,9 +1,24 @@
 import 'mocha';
 import {expect} from 'chai';
-import {Index} from '../src/index';
+import {ImperialLength} from '../src/index';
 
-describe('', () => {
-  it('', () => {
-    expect(Index()).to.be.eql('index');
+const example1 = new ImperialLength(35);
+const example2 = new ImperialLength(400);
+const example3 = new ImperialLength(63360);
+
+describe('ImperialLength Class Tests', () => {
+  it('getLength getter', () => {
+    expect(example1.getLength()).to.be.eql(35);
+    expect(example2.getLength()).to.be.eql(400);
+  });
+  it('toFt  method', () => {
+    expect(example1.toFt()).to.be.eql(3);
+    expect(example2.toFt()).to.be.eql(33);
+  });
+  it('toYd method', () => {
+    expect(example3.toYd()).to.be.eql(1760);
+  });
+  it('toMillas method', () => {
+    expect(example3.toMillas()).to.be.eql(1);
   });
 });
